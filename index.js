e040f798041fdc488f33abec5d38c45a1e41c18a
Arjon200141 <aorjon123@gmail.com>
2024-08-17 02:23:19 +0600

    Bikes are loaded

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const bikeCollection = client.db('bike-hub').collection('bikes');
 
@@ -32,7 +32,6 @@ async function run() {
         const result = await bikeCollection.find().toArray();
         res.send(result);
     })
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
