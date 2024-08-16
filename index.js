f895716dbad8f74f1ada5484a9ee3c56048ba987
Arjon200141 <aorjon123@gmail.com>
2024-08-16 23:34:35 +0600

    Middleware Added

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ej6qyrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
